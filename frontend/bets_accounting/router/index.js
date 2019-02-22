839d572b6682cc382931adab0090baa2e301fb36
Mayurifag <farazeus@gmail.com>
2019-02-22 06:41:47 +0300

    Move Kelly.vue from 'components' to 'pages'

--- frontend/bets_accounting/router/index.js
+++ frontend/bets_accounting/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Kelly from "../components/Kelly.vue";
+import Kelly from "../pages/Kelly.vue";
 import BetsList from "../pages/BetsList.vue";
 
 Vue.use(VueRouter);
