3f694f1ce800aa5141a5d10fe78608e66fa34cef
Mayurifag <farazeus@gmail.com>
2019-08-12 01:05:04 +0300

    WIP: This bunny should be killed.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string           default(""), not null
+#  name            :string
+#  password_digest :string
+#  username        :string           default(""), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
+
 class User < ApplicationRecord
   has_secure_password
 
