3f79dbeb066520f62675e28fe249475f022b256e
Mayurifag <farazeus@gmail.com>
2019-10-26 07:13:49 +0300

    Add pagy gem with configuration

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
   include Knock::Authenticable
+  include Pagy::Backend
   # before_action :authorize_request
 end
