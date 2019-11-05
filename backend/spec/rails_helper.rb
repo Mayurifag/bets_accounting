51a81f5d7a241256f361448bb4d8d490241c48ec
Mayurifag <farazeus@gmail.com>
2019-11-05 19:52:04 +0300

    Update rails 6.0.0 -> 6.0.1

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 
 require 'rspec/rails'
 require 'simplecov'
+require 'shoulda/matchers'
 require 'database_rewinder'
 
 # parallel specs
